d802e07ec560481bcb64f4e438fb0a51bd4d1e08
Ismael-Ort <iroa0001@ce.pucmm.edu.do>
2025-08-07 20:39:37 -0400

    Empezando desde 0

--- proyecto/backend/repositories/ObraRepository.java
+++
@@ -1,8 +0,0 @@
-package proyecto.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import proyecto.backend.models.Obra;
-
-public interface ObraRepository extends JpaRepository<Obra, Long> {
-    // Additional query methods can be defined here if needed
-}
